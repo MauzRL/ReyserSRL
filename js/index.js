a6ade7d1feb6bd3f61443f5667d3cfffb22718e0
Mauri <maurijunk02@gmail.com>
2023-03-09 23:32:59 -0300

    header

--- js/index.js
+++ js/index.js
@@ -18,3 +18,19 @@ back.addEventListener("click", () => {
     front.classList.add('show')
     front.classList.remove('hide')
 })
+
+ /*Cambiar color del menu al hacer Scroll*/
+
+ window.onscroll = function(){
+
+    scroll = document.documentElement.scrollTop;
+
+    header = document.querySelector("header");
+
+    if (scroll > 100){
+        header.classList.add('header_active');
+    }else if (scroll < 100){
+        header.classList.remove('header_active');
+    }
+
+}
